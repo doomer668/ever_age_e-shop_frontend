0d15e278dcee966dffbd099dacc6808316d57006
Damir Bolatov <boldamir0@gmail.com>
2026-01-29 21:40:54 +0100

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { products } from "./data/products";
 import { CartProvider } from "./context/CartContext";
 import { Header } from "./components/Header/Header";
 import  InfoPage  from "./pages/InfoPage/InfoPage";
-import { HomePage } from "./pages/HomePage/HomePage";
+// import { HomePage } from "./pages/HomePage/HomePage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage.tsx";
 import CollabPage from "./pages/CollabPage/CollabPage.tsx";
@@ -27,13 +28,12 @@ const App: React.FC = () => {
           <Header />
           <main style={{ paddingTop: headerHeight }}>
             <Routes>
-              <Route path="/" element={<HomePage />} />
+              <Route path="/" element={<Navigate to={`/product/${products[0].id}`} replace />} />
               <Route path="/info" element={<InfoPage />} />
-              <Route path="*" element={<HomePage />} />
               <Route path="/product/:id" element={<ProductPage />} />
               <Route path="/checkout" element={<CheckoutPage />} />
-              <Route path="/info" element={<InfoPage />} />
               <Route path="/collab/:id" element={<CollabPage />} />
+              <Route path="*" element={<Navigate to={`/product/${products[0].id}`} replace />} />
             </Routes>
           </main>
         </CartProvider>
