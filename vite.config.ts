2e8d95f5db31ccff9a4a325a7218bba0995f3115
dgvspammer <vellenore@gmail.com>
2026-02-01 13:49:04 +0100

    Add visualizer plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      visualizer({
+        open: true,
+        filename: 'dist/stats.html',
+        gzipSize: true,
+      })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
