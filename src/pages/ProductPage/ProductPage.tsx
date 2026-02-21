9350a75a94d6322393226fc27cb5c2b66cbbb18d
goosza <vellenore@gmail.com>
2026-02-21 21:32:17 +0100

    Fix build

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { productApi }  from "../../services/api.ts";
-import { useCart } from "../../context/CartContext";
-import type { Product } from "../../data/products.ts";
+import { productApi }  from "@/services/api.ts";
+import { useCart } from "@/hooks/useCart";
+import type { Product } from "@/data/products.ts";
 import "./ProductPage.css";
 
 const ProductPage: React.FC = () => {
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default {
+// https://vite.dev/config/
+export default defineConfig({
   server: {
     proxy: {
       '/api': {
@@ -12,11 +13,7 @@ export default {
         rewrite: (path: string) => path.replace(/^\/api/, '/api')
       }
     }
-  }
-}
-
-// https://vite.dev/config/
-export default defineConfig({
+  },
   plugins: [
       react(),
       visualizer({
